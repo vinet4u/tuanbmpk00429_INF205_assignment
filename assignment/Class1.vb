5f43dbd342421b4fc3485deb9592685e8aff29d4
vinet4u <vinet4u@gmail.com>
2016-03-04 17:11:23 +0700

    commit

---
+++ assignment/Class1.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data
+Public Class Class1
+    Public Function Loadkhachang() As DataSet
+        Dim chuoiketnoi As String = "workstation id=tuanbmpk00429.mssql.somee.com;packet size=4096;user id=tuanbmpk00429_SQLLogin_1;pwd=b67bvem1m8;data source=tuanbmpk00429.mssql.somee.com;persist security info=False;initial catalog=tuanbmpk00429"
+        Dim conn As SqlConnection = New SqlConnection(chuoiketnoi)
+        Dim LoadKH As New SqlDataAdapter("select MAKH as 'Mã KH' ,TENKH as 'Tên Khách Hàng', DIACHI as 'Địa chỉ', SDT as 'SĐT', EMAIL from KHACHANG", conn)
+        Dim db As New DataSet
+        conn.Open()
+        LoadKH.Fill(db)
+        conn.Close()
+        Return db
+    End Function
+    Public Function Loadsanpham() As DataSet
+        Dim chuoiketnoi As String = "workstation id=tuanbmpk00429.mssql.somee.com;packet size=4096;user id=tuanbmpk00429_SQLLogin_1;pwd=b67bvem1m8;data source=tuanbmpk00429.mssql.somee.com;persist security info=False;initial catalog=tuanbmpk00429"
+        Dim conn As SqlConnection = New SqlConnection(chuoiketnoi)
+        Dim LoadSP As New SqlDataAdapter("select SANPHAM.MASP as 'Mã sản phẩm',SANPHAM.TENSP as 'Tên sản phẩm', LOAISANPHAM.MALOAI as 'Mã Loại', LOAISANPHAM.TENLOAI as 'Tên Loại',SANPHAM.SOLUONG as 'Số lượng' from SANPHAM inner join LOAISANPHAM on SANPHAM.MASP = LOAISANPHAM.MASP", conn)
+        Dim db As New DataSet
+        conn.Open()
+        LoadSP.Fill(db)
+        conn.Close()
+        Return db
+    End Function
+End Class
